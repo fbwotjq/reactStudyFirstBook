d998a9381b71590fbcfb9f1662d0fc23dda679ce
jaco <fbwotjq@gmail.com>
2018-09-22 21:53:37 +0900

    라이프 싸이클 관련 스터디 코드 추가

--- component-life-cycle-example/src/App.js
+++ component-life-cycle-example/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LifeCycleSample from './LifeCycleSample'
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() + 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <button onClick={this.handleClick}>랜덤색상</button>
+       <LifeCycleSample color={this.state.color}/>
       </div>
     );
   }
