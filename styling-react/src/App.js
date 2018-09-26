fd63dedccb59359254d3e6e8733922cc3437544c
jaco <fbwotjq@gmail.com>
2018-09-26 15:28:45 +0900

    스타일 관련 처리 예제 추가

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import classNames from 'classnames/bind'
-import styles from './App.css';
+//import styles from './App.css';
+import styles from './App.scss';
+import Button from './components/Button';
 
 console.log(styles);
 const cx = classNames.bind(styles);
 
 class App extends Component {
   render() {
-    const isBlue = false;
+    const isBlue = true;
     return (
       //<div className={[styles.box, styles.blue].join(' ')}></div>
       //<div className={cx('box', 'blue')}></div>
-      <div className={cx('box', { blue: isBlue})}></div>
+      <div className={cx('box', { blue: isBlue })}>
+        <div className={cx('box-inside')}></div>
+        {/*<Button>버튼</Button> */}
+      </div>
     );
   }
 }
