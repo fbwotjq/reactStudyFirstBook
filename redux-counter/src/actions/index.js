28e6f91e7dee252e0e906c24c2afc4e04f757b80
jaco <fbwotjq@gmail.com>
2018-10-01 00:55:06 +0900

    리덕스 첫번쨰 적용 예제

---
+++ redux-counter/src/actions/index.js
@@ -0,0 +1,5 @@
+import * as types from  './ActionTypes';
+
+export const increment = () => ({type: types.INCREMENT});
+export const decrement = () => ({type: types.DECREMENT});
+export const setColor = (color) => ({type: types.SET_COLOR, color});
