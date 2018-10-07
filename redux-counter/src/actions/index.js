7e251ff168f54dc462681d591b26664b92bb4632
jaco <fbwotjq@gmail.com>
2018-10-07 13:50:42 +0900

    멀티 카운터 예제 추가

--- redux-counter/src/actions/index.js
+++ redux-counter/src/actions/index.js
@@ -1,5 +1,7 @@
 import * as types from  './ActionTypes';
 
-export const increment = () => ({type: types.INCREMENT});
-export const decrement = () => ({type: types.DECREMENT});
-export const setColor = (color) => ({type: types.SET_COLOR, color});
+export const create = (color) => ({type: types.CREATE, color});
+export const remove = () => ({type: types.REMOVE});
+export const increment = (index) => ({type: types.INCREMENT, index});
+export const decrement = (index) => ({type: types.DECREMENT, index});
+export const setColor = (color, index) => ({type: types.SET_COLOR, index, color});
